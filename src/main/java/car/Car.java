d28b832cabaa3510e56e9e74210ca5c9e5863a4c
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-15 17:10:09 +0900

    전진기능

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -1,8 +1,12 @@
 package car;
 
+import lombok.Getter;
+
+@Getter
 public class Car {
     private final String name;
     private final Integer MAX_LENGTH = 5;
+    public Integer distance = 0;
 
     public Car (String carName) throws Exception{
         this.name = carName;
@@ -12,6 +16,12 @@ public class Car {
     private void validateName() throws Exception {
         if (name.length() > MAX_LENGTH) {
             throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다.");
+        }
+    }
+
+    public void drive(Integer probability) {
+        if (probability > 4) {
+            distance += 1;
         }
     }
 
--- src/test/java/car/CarTest.java
+++ src/test/java/car/CarTest.java
@@ -3,6 +3,8 @@ package car;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 class CarTest {
 
     @Test
@@ -12,15 +14,19 @@ class CarTest {
 
     @Test
     void 이름_5자_이상_예외발생_Test() throws Exception{
-        Assertions.assertThatThrownBy(() -> {
+        assertThatThrownBy(() -> {
             Car car = new Car("5자이상입니다");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("자동차의 이름은 5자를 초과할 수 없습니다.");
     }
 
     @Test
-    void 전진기능테스트_자동차이름_동시출력() {
+    void 전진기능테스트_전진() throws Exception{
+        Car car = new Car("poli");
+        car.drive(5);
+        Integer distance = car.distance;
 
+        assertThat(distance).isEqualTo(1);
     }
 
     @Test
