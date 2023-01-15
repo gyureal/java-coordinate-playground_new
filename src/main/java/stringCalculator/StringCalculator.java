5b7589fc802ae274555eeb4cab79ac49b6cb11ab
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-01-15 12:41:26 +0900

    split 로직변경(|) 세미콜론-> 콜론

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -6,29 +6,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
-    private final String inputString;
-    private static final String[] default_separator = {",", ":"};
+    private String inputString;
+    private String customSeparator;
 
     public StringCalculator(String inputString) {
         this.inputString = inputString;
     }
 
-    public List<Integer> split(){
-        String[] splitArray = inputString.split("");
-        List<String> splitList = Arrays.asList(splitArray);
-        return splitList.stream()
-                .filter(s -> !s.equals(","))
-                .filter(s -> !s.equals(";"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+    public Boolean isNull(String text) {
+        if (text == null || text.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String[] split(){
+        return inputString.split(",|:");
     }
 
     public Integer sum() {
-        List<Integer> split = split();
-        return split.stream().reduce(0, Integer::sum);
+        String[] split = split();
+        return Arrays.asList(split).stream()
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
-
 }
 // asList로 생성한 arrayList remove가 안되는 이유와 해결책
 // java.lang.UnsupportedOperationException: remove
-//https://kkwonsy.tistory.com/14
+// https://kkwonsy.tistory.com/14
+
+// stream 값 sum 하기
+// https://kapentaz.github.io/java/Java8-stream-sum-%EA%B5%AC%ED%95%98%EA%B8%B0/#
--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -14,45 +14,33 @@ class StringCalculatorTest {
     @Test
     void splitTest_컴마() {
         StringCalculator sc = new StringCalculator("1,2,3,4");
-        List<Integer> result = sc.split();
+        String[] result = sc.split();
 
-        List<Integer> answer = new ArrayList<>();
-        answer.add(1);
-        answer.add(2);
-        answer.add(3);
-        answer.add(4);
+        String[] answer = {"1","2","3","4"};
         assertThat(result).isEqualTo(answer);
     }
 
     @Test
-    void splitTest_세미콜론() {
-        StringCalculator sc = new StringCalculator("1;2;3;4");
-        List<Integer> result = sc.split();
+    void splitTest_콜론() {
+        StringCalculator sc = new StringCalculator("1:2:3:4");
+        String[] result = sc.split();
 
-        List<Integer> answer = new ArrayList<>();
-        answer.add(1);
-        answer.add(2);
-        answer.add(3);
-        answer.add(4);
+        String[] answer = {"1","2","3","4"};
         assertThat(result).isEqualTo(answer);
     }
 
     @Test
-    void splitTest_컴마와세미콜론() {
-        StringCalculator sc = new StringCalculator("1;2,3;4");
-        List<Integer> result = sc.split();
+    void splitTest_컴마와콜론() {
+        StringCalculator sc = new StringCalculator("1:2,3:4");
+        String[] result = sc.split();
 
-        List<Integer> answer = new ArrayList<>();
-        answer.add(1);
-        answer.add(2);
-        answer.add(3);
-        answer.add(4);
+        String[] answer = {"1","2","3","4"};
         assertThat(result).isEqualTo(answer);
     }
 
     @Test
     void sumTest_10() {
-        StringCalculator sc = new StringCalculator("1;2,3;4");
+        StringCalculator sc = new StringCalculator("1:2,3:4");
         Integer sum = sc.sum();
         assertThat(sum).isEqualTo(10);
     }
