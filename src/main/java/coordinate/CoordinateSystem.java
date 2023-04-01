7b15058eea53a020a2372a59147bb0637cb1c834
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-01 16:25:10 +0900

    좌표계 클래스 추가, 좌표계 테스트 추가, 좌표계 표시 메서드, 거리 프린트 메서드

--- src/main/java/coordinate/CoordinateSystem.java
+++ src/main/java/coordinate/CoordinateSystem.java
@@ -2,6 +2,36 @@ package coordinate;
 
 public class CoordinateSystem {
 
+    private static final int GRID_LIMIT = 24;
+
+    private final Coordinates coordinates;
+
+    public CoordinateSystem() {
+        this.coordinates = new Coordinates();
+    }
+
+    public void addCoordinate(Coordinate coordinate) {
+        coordinates.addCoordinate(coordinate);
+    }
+
+    public void rendering() {
+        String result = "";
+        result += makeGrid();
+        result += "\n";
+        result += printDistance();
+        System.out.println(result);
+    }
+
+    private String printDistance() {
+        double distance = 3d;
+        return String.format("두 점 사이의 거리는 %f", distance);
+    }
+
+    public String makeGrid() {
+        String entireLine = "";
+
+        return entireLine;
+    }
 
 
 }
---
+++ src/test/java/coordinate/CoordinateSystemTest.java
@@ -0,0 +1,14 @@
+package coordinate;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoordinateSystemTest {
+
+    @Test
+    void makeGridTest() {
+
+    }
+
+}
