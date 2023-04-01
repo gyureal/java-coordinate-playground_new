36f4a617c9bc1bcbadc8d66c63d84512d508066d
yonggyujeong <yonggyu.jeong10@gmail.com>
2023-04-01 16:23:59 +0900

    좌표 일급컬렉션에 좌표 추가 메서드 추가

--- src/main/java/coordinate/Coordinates.java
+++ src/main/java/coordinate/Coordinates.java
@@ -10,6 +10,12 @@ public class Coordinates {
         this.coordinates = coordinates;
     }
 
+    public Coordinates() {this.coordinates = new ArrayList<>(); }
+
+    public void addCoordinate(Coordinate coordinate) {
+        coordinates.add(coordinate);
+    }
+
     public double calculateDistance() {
         if (coordinates.size() != 2) {
             throw new IllegalStateException("거리를 구하려면 좌표가 두개 여야 합니다.");
